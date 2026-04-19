587ad3641653ec86324e6d04fc8e123d2ffa7de2
laala <hamdaouilaala2@gmail.com>
2026-04-19 16:21:24 -0400

    creation de la class salle

---
+++ models/salle.py
@@ -0,0 +1,9 @@
+class Salle:
+    def __init__(self, code, libelle, type, capacite):
+        self.code = code
+        self.libelle = libelle
+        self.type = type
+        self.capacite = capacite
+
+    def afficher_infos(self):
+        return f"Code: {self.code}, Libellé: {self.libelle}, Type: {self.type}, Capacité: {self.capacite}"
