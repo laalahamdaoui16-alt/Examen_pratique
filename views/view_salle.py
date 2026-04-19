f7acfe8609ad9be3f8a48b6c997a7338baafbf70
laala <hamdaouilaala2@gmail.com>
2026-04-19 17:08:28 -0400

    ajout methode modifier salles

--- views/view_salle.py
+++ views/view_salle.py
@@ -109,6 +109,7 @@ class ViewSalle(ctk.CTk):
         else:
             messagebox.showerror("Erreur", "Salle introuvable")
 
+
     def lister_salles(self):
         self.treeList.delete(*self.treeList.get_children())
         liste = self.service_salle.recuperer_salles()
